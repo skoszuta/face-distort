e97d1a083ccb7067df1edb8a5c941729fa4d8a01
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2021-10-25 21:21:59 +0200

    position camera selection in the top-right corner

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,41 +1,56 @@
+import './settings.scss'
+
 const Settings = {
-    rootEl: document.createElement('aside'),
-    state: null,
-    visible: false,
-    async init(state) {
-        Settings.state = state
+  rootEl: document.createElement('aside'),
+  state: null,
+  visible: false,
+  async init(state) {
+    Settings.state = state
 
-        const devices = await navigator.mediaDevices.enumerateDevices()
-        await Settings.setCamera(devices.filter((device) => device.kind === 'videoinput')[0].deviceId)
+    const devices = await navigator.mediaDevices.enumerateDevices()
+    await Settings.setCamera(
+      devices.filter((device) => device.kind === 'videoinput')[0].deviceId
+    )
 
-        const cameraOptions = await renderCameraOptions(devices)
-        Settings.rootEl.innerHTML = `
+    const cameraOptions = await renderCameraOptions(devices)
+    Settings.rootEl.innerHTML = `
             <select>
                 <option value="" disabled selected>Camera</option>
                 ${cameraOptions}
             </select>
         `
-        Settings.rootEl.getElementsByTagName('select')[0].addEventListener('change', Settings.handleCameraChange)
+    Settings.rootEl
+      .getElementsByTagName('select')[0]
+      .addEventListener('change', Settings.handleCameraChange)
 
-        document.body.appendChild(Settings.rootEl)
-    },
-    handleCameraChange: (e) => {
-        Settings.setCamera(e.target.value)
-    },
-    handleKeyDown: (e) => {
+    Settings.rootEl.classList.add('settings')
 
-    },
-    setCamera: async (deviceId) => {
-        Settings.state.camera = deviceId
-        Settings.state.stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: Settings.state.camera }})
-        Settings.state.imageCapture = new ImageCapture(Settings.state.stream.getVideoTracks()[0])
-    }
+    document.body.appendChild(Settings.rootEl)
+  },
+  handleCameraChange: (e) => {
+    Settings.setCamera(e.target.value)
+  },
+  handleKeyDown: (e) => {},
+  setCamera: async (deviceId) => {
+    Settings.state.camera = deviceId
+    Settings.state.stream = await navigator.mediaDevices.getUserMedia({
+      video: { deviceId: Settings.state.camera },
+    })
+    Settings.state.imageCapture = new ImageCapture(
+      Settings.state.stream.getVideoTracks()[0]
+    )
+  },
 }
 
 async function renderCameraOptions(devices) {
-    return devices
+  return devices
     .filter((device) => device.kind === 'videoinput')
-    .map((device) => `<option value="${device.deviceId}" ${Settings.state.camera === device.deviceId ? 'selected' : ''}>${device.label}</option>`)
+    .map(
+      (device) =>
+        `<option value="${device.deviceId}" ${
+          Settings.state.camera === device.deviceId ? 'selected' : ''
+        }>${device.label}</option>`
+    )
     .join('')
 }
 
